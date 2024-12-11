dcc3b27590dc46d87428d6fe18d81f87ca30a47e
Faizal Khan <faizalkhn98641@gmail.com>
2024-12-11 21:11:34 +0530

    fix-zcb-bond

--- app/api/manifest_routes.py
+++ app/api/manifest_routes.py
@@ -384,7 +384,7 @@ def transaction_manifest_routes(app):
                             Decimal("{req.notional_principal}")
                             {req.discount}u64
                             "{req.bond_position}"
-                            u64{req.bond_price}
+                            u64{int(req.bond_price)}
                             Decimal("{req.number_of_bonds}")
                             Address("{req.user_address}")
                             Bucket("bucket1")
