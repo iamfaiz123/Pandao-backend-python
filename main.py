5ca799fefb77831a96c81bbd1545afae348c5bc3
Faizal Khan <faizalkhn98641@gmail.com>
2024-12-27 20:55:18 +0530

    update-cors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from app.api.manifest_routes import transaction_manifest_routes
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["pandao.live"],  # Allow all origins
+    allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
