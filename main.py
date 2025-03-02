678f1d13e171c532abe081dc671c5aee35423b68
iamfaiz123 <dherij@spinloom.com>
2025-03-02 14:19:21 +0530

    fix-cors

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 
